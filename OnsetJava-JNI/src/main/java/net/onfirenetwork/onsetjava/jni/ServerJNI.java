9a97c2e58ca9f120c59dfdeb97970812a6cdf2c1
Jan Bebendorf <jan@bebendorf.eu>
2020-07-08 16:08:34 +0200

    Update ServerJNI.java

--- OnsetJava-JNI/src/main/java/net/onfirenetwork/onsetjava/jni/ServerJNI.java
+++ OnsetJava-JNI/src/main/java/net/onfirenetwork/onsetjava/jni/ServerJNI.java
@@ -188,7 +188,10 @@ public class ServerJNI implements Server {
     }
 
     public WorldObject createObject(double x, double y, double z, int model){
-        return new WorldObjectJNI((Integer) callGlobal("CreateObject", model, x, y, z)[0]);
+        Object id = callGlobal("CreateObject", model, x, y, z)[0];
+        if(id instanceof Integer)
+            return new WorldObjectJNI((Integer) id);
+        return new WorldObjectJNI(Integer.parseInt((String) id));
     }
 
     public Vehicle createVehicle(double x, double y, double z, double heading, int model){
